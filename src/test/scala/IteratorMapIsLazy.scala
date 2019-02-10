58c09cea39d066ba92d348ab7c39c577b1d4c0cb
Konstantin Läufer <klaeufer@gmail.com>
2019-02-10 12:00:19 +0100

    rearranged test into when-should format
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/test/scala/IteratorMapIsLazy.scala
+++ src/test/scala/IteratorMapIsLazy.scala
@@ -1,27 +1,34 @@
 import org.mockito.IdiomaticMockito
 import org.scalatest.{ Matchers, WordSpec }
 
-class IteratorMapIsLazy extends WordSpec with IdiomaticMockito with Matchers {
+class IteratorMapIsLazy extends WordSpec with Matchers with IdiomaticMockito {
 
-  "Iterator.map" should {
+  "Iterator.map" when {
 
-    "be lazy when tested via mutable state" in {
-      var counter = 0
-      val it = Iterator.continually {
-        counter += 1; counter
+    "tested using mutable state" should {
+
+      "be lazy" in {
+        var counter = 0
+        val it = Iterator.continually {
+          counter += 1
+          counter
+        }
+        val result = it.map(_ + 1)
+        counter shouldBe 0
+        result.next()
+        counter shouldBe 1
       }
-      val result = it.map(_ + 1)
-      counter shouldBe 0
-      result.next()
-      counter shouldBe 1
     }
 
-    "be lazy when tested via a spy" in {
-      val it = spy(Iterator.continually("hello"))
-      val result = it.map(_.length)
-      it.next() wasNever called
-      result.next()
-      it.next() wasCalled once
+    "when tested via a spy" should {
+
+      "be lazy" in {
+        val it = spy(Iterator.continually("hello"))
+        val result = it.map(_.length)
+        it.next() wasNever called
+        result.next()
+        it.next() wasCalled once
+      }
     }
   }
 }
